f6d4f6a0cbecdb1b53e473d03e820485d2cb16a2
Chris <Christopherjorge47@icloud.com>
2024-03-18 13:41:14 -0400

    Completed third question

--- app.js
+++ app.js
@@ -22,16 +22,53 @@ const maxOfThreeNumbers = (num1, num2, num3) =>{
     this.num1 = num1;
     this.num2 = num2;
     this.num3 = num3;
-    let arr = [num1,num2,num3]
-    let max = 0
+    let arr = [num1,num2,num3];
+    let max = 0;
     for (let i = 0; i < arr.length; i++)
     {
         if (arr[i] > max)
         {
             max = arr[i];
-        }
+        };
+    };
+    return max;
+};
+
+const isCharAVowel = (char) =>
+{
+    this.char = char
+    char = char.toLowerCase()
+    
+
+    if(char === 'a'){
+        return true;
     }
-    return max
+    else if(char === 'e')
+    {
+        return true;
+    }
+    else if(char === 'i')
+    {
+        return true;
+    }
+    else if(char === 'o')
+    {
+        return true;
+    }
+    else if(char === 'u')
+    {
+        return true;
+    }
+    else if(char === 'y')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
 }
 // console.log(maxOfTwoNumbers(5,5))
 console.log(maxOfThreeNumbers(9,40,5))
+console.log(isCharAVowel('A'))
