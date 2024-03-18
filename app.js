07b99459256bfe2b0b57611e4b59447ea9d7b2fd
Chris <Christopherjorge47@icloud.com>
2024-03-18 13:34:57 -0400

    Completed second question

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const maxOfTwoNumbers = (num1, num2) =>{
+const maxOfTwoNumbers = (num1, num2) =>
+{
     this.num1 = num1;
     this.num2 = num2;
     
@@ -13,6 +14,24 @@ const maxOfTwoNumbers = (num1, num2) =>{
     else
     {
         return num1;
-    }
+    };
 
+};
+
+const maxOfThreeNumbers = (num1, num2, num3) =>{
+    this.num1 = num1;
+    this.num2 = num2;
+    this.num3 = num3;
+    let arr = [num1,num2,num3]
+    let max = 0
+    for (let i = 0; i < arr.length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max
 }
+// console.log(maxOfTwoNumbers(5,5))
+console.log(maxOfThreeNumbers(9,40,5))
